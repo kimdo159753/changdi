e0760a0f26a4e8fac3c971da9724318ba56d7d9f
ddobba <kimdo159753@gmail.com>
2025-10-29 10:27:16 +0900

    frontend with AI

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return "emotion to music!"
+def home():
 
-if __name__ == "__main__":
+    return render_template('index.html')
+
+if __name__ == '__main__':
     app.run(debug=True)
